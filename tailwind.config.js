2a90d5717fe8d0e0f9025ebac29abdfdb8686196
Fidele K.Cyisa <fihacker000@gmail.com>
2021-11-26 13:28:54 +0300

    adding django-tailwing-starter

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './templates/*.html',
+    './templates/**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
